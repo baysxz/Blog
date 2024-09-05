2a897248e2283907a5490615671bb17b7ddbd6a5
baysxz <baysgalanxz@gmail.com>
2024-09-05 23:57:24 +0800

    a

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,20 +7,22 @@ import { Twitter } from "@/Icons/Twitter";
 
 export const Footer = () => {
   return (
-    <div className=" grid grid-cols-3 bg-gray-50 px-[352px]">
-      <div className="w-[280px] h-[160px]">
-        <h1 className="text-lg font-semibold pb-4">About</h1>
-        <p className="text-base font-normal pb-6 ">
+    <div className=' grid grid-cols-3 gap-6 bg-gray-50 px-[352px]'>
+      <div className='w-[280px] h-[160px]'>
+        <h1 className='text-lg font-semibold pb-4'>About</h1>
+        <p className='text-base font-normal pb-6 '>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam
         </p>
-        <div className="text-base font-semibold">
+        <p>Email : info@jstemplate.net</p>
+        <p>Phone : 880 123 456 789</p>
+        {/* <div className='text-base font-semibold'>
           <p>Email : info@jstemplate.net</p>
           <p>Phone : 880 123 456 789</p>
-        </div>
+        </div> */}
       </div>
-      <div className="flex flex-col justify-center text-start gap-2">
+      <div className='flex flex-col justify-center text-start gap-2 px-[110px]'>
         <ul>
           <li>Home</li>
           <li>Blog</li>
@@ -28,13 +30,13 @@ export const Footer = () => {
         </ul>
       </div>
 
-      <div className="flex gap-5">
+      <div className='flex gap-5'>
         <Facebook />
         <Twitter />
         <Insta />
         <Linkedin />
       </div>
-      <div>
+      <div className='flex'>
         <FooterLogo />
         <FoooterTextLogo />
       </div>
