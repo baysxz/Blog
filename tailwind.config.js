1a8a6aeaabe50772e778e8001fd11933106a4fda
baysxz <baysgalanxz@gmail.com>
2024-09-16 13:46:32 +0800

    aa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
-    },
-  },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {},
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
